4144c355010dc06a3feea4d65a94222cdbf14c69
MudayJ <02jensen.muday@gulllakecs.org>
2023-10-16 08:11:27 -0400

    Add a print statement

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -5,4 +5,5 @@
 print("hello Jensen Muday")
 print("")
 print("\n I am looking forward to my birthday on october 43rd")
-print("I will beat micky dees one day")
+print("I will beat micky")
+print("Yo quiero taco bell got stuck in my head again after thinking about that chihuahua for 3 weeks straight.")
